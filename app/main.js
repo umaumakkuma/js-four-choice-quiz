fc2bca6c829763babf754d187bf31014650cd6d0
Yusuke.Nomura <umaumakkuma@gmail.com>
2020-01-23 01:50:19 +0900

    update

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 (function () {
   'use strict';
+
   // 問題定義
   const questionsData = [
     {
@@ -18,29 +19,28 @@
       'id': 3,
       'text': '鰯の読み方は？',
       'choice': ['くじら', 'いわし', 'まぐろ', 'ひらめ'],
-      'answer': 0
+      'answer': 1
     }
   ];
 
-  const $openingWrap = $('#js-opening-wrap');
-  const $questionWrap = $('#js-question-wrap');
-  const $resultWrap = $('#js-result-wrap');
-  let questionCount = 0;
-  let $questionNum = $('#js-question-num');
-  let $questionText = $('#js-question-text');
-  const $answerArea = $('#js-answer-area');
-  let answerList = [];
-  let correctAnswerList = [];
-  let selectAnswerList = [];
+  const $openingWrap   = $('#js-opening-wrap');  // オープニング画面
+  const $questionWrap  = $('#js-question-wrap'); // 問題画面
+  const $resultWrap    = $('#js-result-wrap');   // リザルト画面
+  let questionCount    = 0;                      // 問題番号(0を1番目とする)
+  let answerArr        = [];                     // 選択肢
+  let correctAnswerArr = [];                     // 正解選択肢
+  let selectAnswerArr  = []                     // ユーザーが選んだ選択肢
 
   // 画面初期化
   function init() {
+    // オープニングを表示させそれ以外を非表示にする
     $openingWrap.show();
     $questionWrap.hide();
     $resultWrap.hide();
 
-    $('#js-btn-start').on('click', function() {
-      $openingWrap.fadeOut(500, function() {
+    // 開始するボタン押下で問題画面に切り替え、問題を表示させる
+    $('#js-btn-start').on('click', function () {
+      $openingWrap.fadeOut(500, function () {
         $questionWrap.fadeIn(500);
         showQuestion();
       });
@@ -49,22 +49,26 @@
 
   // 問題表示
   function showQuestion() {
+    const $questionNum  = $('#js-question-num');  // 問題番号の画面表示
+    const $questionText = $('#js-question-text'); // 問題文の画面表示
+    const $answerArea   = $('#js-answer-area');   // 選択肢表示領域
+
     // 問題番号表示(第x問)
-    $questionNum.html(questionsData[questionCount]['id']);
+    $questionNum.text(questionsData[questionCount]['id']);
 
     // 問題文表示
-    $questionText.html(questionsData[questionCount]['text']);
+    $questionText.text(questionsData[questionCount]['text']);
 
     // 四択回答初期化&生成表示
-    answerList = [];
+    answerArr = [];
     for (let i = 0; i < questionsData[questionCount]['choice'].length; i++) {
-      answerList.push(`<li class="choice" data-id=${i}>${questionsData[questionCount]['choice'][i]}</li>`);
+      answerArr.push(`<li class="choice" data-id=${i}>${questionsData[questionCount]['choice'][i]}</li>`);
     }
-    $answerArea.html(answerList.join(''));
+    $answerArea.html(answerArr.join(''));
 
     // 正解番号
-    correctAnswerList.push(questionsData[questionCount]['answer']);
-console.log(correctAnswerList, 'コレクトアンサー');
+    correctAnswerArr.push(questionsData[questionCount]['answer']);
+
     // 問題番号インクリメント
     questionCount++;
 
@@ -73,21 +77,60 @@ console.log(correctAnswerList, 'コレクトアンサー');
 
   // 回答選択
   function choiseAnswer() {
-    let choices = $('.choice');
+    const choices = $('.choice');
+
+    // 選択肢を押下したとき
     choices.on('click', function () {
-    selectAnswerList.push(this.dataset.id);
-    console.log(selectAnswerList, 'チェックアンサー');
+
+      // 押下した選択肢を格納(リザルト画面でスコア表示に使用)
+      selectAnswerArr.push(this.dataset.id);
+
+      // 残り問題数によって分岐
       if (questionsData[questionCount] === undefined) {
-        $('#js-result-total-question').html(questionCount)
-        // コレクトアンサーとセレクトアンサーリストで合っている数を数える
-        // $('#js-result-total-answer').html()
-        $questionWrap.fadeOut(500, function() {
-          $resultWrap.fadeIn(500);
-        });
+        // リザルト画面表示
+        showResult();
       } else {
+        // 次の問題を表示
         showQuestion();
       }
     });
   }
+
+  // リザルト画面
+  function showResult() {
+    // 合計問題数を表示
+    $('#js-result-total-question').text(questionCount)
+
+    // 正解数を計算し表示
+    let resultTotalScore = 0;
+    for (let i = 0; i < correctAnswerArr.length; i++) {
+      if (correctAnswerArr[i] == selectAnswerArr[i]) {
+        resultTotalScore++;
+      }
+    }
+    $('#js-result-total-score').text(resultTotalScore);
+
+    // 正解数に応じたコメントを表示
+    const resultCommentJudge = resultTotalScore / correctAnswerArr.length;
+    let resultComment = '';
+    if (resultCommentJudge === 1) {
+      resultComment = '全問正解！　素晴らしい٩(๑òωó๑)۶';
+    } else if (resultCommentJudge > 0.8) {
+      resultComment = 'あと少しで全問正解！';
+    } else if (resultCommentJudge > 0.5) {
+      resultComment = '頑張りましょう';
+    } else if (resultCommentJudge > 0.3) {
+      resultComment = '精進が足りません';
+    } else {
+      resultComment = 'マジ？';
+    }
+    $('#js-result-comment').text(resultComment);
+
+    // リザルト画面表示
+    $questionWrap.fadeOut(500, function () {
+      $resultWrap.fadeIn(500);
+    });
+  }
+
   init();
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,18 @@ var gulp         = require('gulp'),
     browserSync  = require('browser-sync'),
     sass         = require('gulp-sass'),
     sourcemaps   = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    gifsicle     = require('imagemin-gifsicle'),
-    runSequence  = require('run-sequence'),
-    rimraf       = require('rimraf');
+    autoprefixer = require('gulp-autoprefixer');
 
-var inputDir = 'app',
-    outputDir = 'public_html';
+var inputDir = 'app'
 
 // browser-sync
 gulp.task('browser-sync', function() {
   browserSync({
     notify: false,
     server: {
-       baseDir: inputDir,
-       index  : 'index.html',
-       directory: true
-    }
-  });
-});
-
-// browser-sync:dist
-gulp.task('browser-sync:dist', function() {
-  browserSync({
-    notify: false,
-    server: {
-       baseDir: outputDir,
-       index  : 'index.html',
-       directory: true
+      baseDir: inputDir,
+      index  : 'index.html',
+      directory: true
     }
   });
 });
@@ -49,40 +33,6 @@ gulp.task('sass', function() {
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(inputDir))
-});
-
-// sass:build
-gulp.task('sass:build', function() {
-  return gulp.src('./app/**/*.scss')
-    .pipe(sass({
-        outputStyle : 'expanded'
-    }))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(outputDir))
-});
-
-// remove public_html
-gulp.task('clean', function(cb) {
-  return rimraf(outputDir, cb)
-});
-
-gulp.task('copy', function() {
-  return gulp.src([
-    './app/**/*',
-    '!./app/**/*.map',
-    '!./app/**/*.scss'
-  ])
-  .pipe(gulp.dest(outputDir));
-});
-
-// build
-gulp.task('build', function(cb) {
-  runSequence('clean', ['min-css', 'min-js'], 'copy', ['sass:build', 'sass'], cb);
-});
-
-// build:full
-gulp.task('build:full', function(cb) {
-  runSequence('clean', ['min-css', 'min-js'], 'copy', ['sass:build', 'sass', 'imagemin'], 'purgecss', cb);
 });
 
 // 監視ファイル
